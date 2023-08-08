a94869600363c5802571737c161e0956933f8977
FrancoAlcaraz <franco231000@gmail.com>
2023-08-08 10:53:17 -0300

    agregado

--- DsAPP Ejercicio 2/src/dsapp/ejercicio/pkg2/EJEMPLO.java
+++ DsAPP Ejercicio 2/src/dsapp/ejercicio/pkg2/EJEMPLO.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dsapp.ejercicio.pkg2;
 
-/**
- *
- * @author Usuario
- */
 public class EJEMPLO {
+    public void Hola(){
     
+        System.out.println("Hola");
+    }
 }
