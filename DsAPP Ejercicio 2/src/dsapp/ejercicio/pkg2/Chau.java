3fadeeb5f5a70e385c8dc5bcd3b72660f48e9c0e
alfadev1 <alfarollano@gmail.com>
2023-08-08 11:27:27 -0300

    ej

--- DsAPP Ejercicio 2/src/dsapp/ejercicio/pkg2/Chau.java
+++ DsAPP Ejercicio 2/src/dsapp/ejercicio/pkg2/Chau.java
@@ -9,5 +9,17 @@ package dsapp.ejercicio.pkg2;
  * @author cyka
  */
 public class Chau {
+    int duracion;
+    public Chau(int duracion){
+    this.duracion=duracion;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
     
 }
